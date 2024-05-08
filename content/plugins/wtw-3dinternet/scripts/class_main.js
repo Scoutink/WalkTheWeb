97a8aa7a476ea6da1b2f5be6612b1aae29629ca6
Aaron Dishno Ed.D <adishno@walktheweb.com>
2024-05-08 15:53:38 -0700

    Updated 3.7.6
    Google Analytics Update - We updated the Google Analytics scripts to now handle GA4 triggers.
    
    Removed Ad from Movement Menu - There was a simple ad on the Movement Menu under Help that was being used as a reminder of 3D functionality. We removed the ad.
    
    Reformatted the Admin Dashboard - We reformatted the admin home dashboard page. It now has some minimum widths set, floats to next row as needed, and the page now scrolls vertically as needed.
    
    Set Cookie has been removed from Headers - In the JSON formatted files there was a header for Set-Cookie. We were not using cookies in these files and Google Chrome started to trigger a warning that third party cookies would be denied in an upcoming update. Therefore, we removed the set cookie header reference from all pages. This has removed the related Google Chrome warnings.
    
    Discord Updates - All of the Activity feeds to Discord have been upgraded to the latest webhook methods.

--- content/plugins/wtw-3dinternet/scripts/class_main.js
+++ content/plugins/wtw-3dinternet/scripts/class_main.js
@@ -201,8 +201,8 @@ WTW_3DINTERNET.prototype.openDashboardForm = async function(zshow) {
 					for (var i=0;i<zresponse.length;i++) {
 						if (zresponse[i] != null) {
 							if (i == 0) {
-								dGet('wtw_latestvideotitle').innerHTML = atob(zresponse[i].videotitle);
-								dGet('wtw_latestvideodetails').innerHTML = 'Presented by: ' + zresponse[i].presenter + ' on ' + WTW.formatDate(zresponse[i].updatedate) + '<br /><br />' + atob(zresponse[i].description);
+								dGet('wtw_latestvideotitle').innerHTML = zresponse[i].videotitle;
+								dGet('wtw_latestvideodetails').innerHTML = 'Presented by: ' + zresponse[i].presenter + ' on ' + WTW.formatDate(zresponse[i].updatedate) + '<br /><br />' + zresponse[i].description;
 								if (zresponse[i].videourl.indexOf('?v=') > -1) {
 									var zyoutubeid = zresponse[i].videourl.split('?v=')[1];
 									dGet('wtw_latestvideo').innerHTML = "<iframe width='100%' height='auto' src='https://www.youtube.com/embed/" + zyoutubeid + "?list=PLnMgA5ebbr8KXw9z5vp4E202e-RTKa9X-' frameborder='0' allowfullscreen style='min-height:350px;' title='WalkTheWeb Video' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'></iframe>";
